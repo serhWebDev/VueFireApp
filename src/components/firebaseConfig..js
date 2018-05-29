b06dd93ad2bb69868a6ddeae35d5d1b6e9e716fe
Serhii Vekshyn <serhiivekshyn@gmail.com>
2018-05-29 22:22:11 +0300

    new May
    new May

---
+++ src/components/firebaseConfig..js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCBYLVl30qL0hgbC7QNHGlawnhygh-zGtc",
+    authDomain: "vfsapp-71529.firebaseapp.com",
+    databaseURL: "https://vfsapp-71529.firebaseio.com",
+    projectId: "vfsapp-71529",
+    storageBucket: "vfsapp-71529.appspot.com",
+    messagingSenderId: "388295993457"
+}
