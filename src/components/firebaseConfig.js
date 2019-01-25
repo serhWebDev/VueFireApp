e734faa3f852af66930902bec6a599973c7d4bff
Serhii VekVekVek <serhiivekshyn@gmail.com>
2019-01-26 01:25:07 +0200

    Delete firebaseConfig.js

--- src/components/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyDZ4k-HHxstsvPQUkDoSS2l5uOo5SkOsoQ",
-    authDomain: "offerwallempmanager.firebaseapp.com",
-    databaseURL: "https://offerwallempmanager.firebaseio.com",
-    projectId: "offerwallempmanager",
-    storageBucket: "offerwallempmanager.appspot.com",
-    messagingSenderId: "897520998208"
-}
