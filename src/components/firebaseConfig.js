edefe33e0763a4227f260b8f4e65567461c50311
Serhii Vekshyn <serhiivekshyn@gmail.com>
2019-02-24 16:41:42 +0200

    firebaseConfig

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyDZ4k-HHxstsvPQUkDoSS2l5uOo5SkOsoQ",
+    authDomain: "offerwallempmanager.firebaseapp.com",
+    databaseURL: "https://offerwallempmanager.firebaseio.com",
+    projectId: "offerwallempmanager",
+    storageBucket: "offerwallempmanager.appspot.com",
+    messagingSenderId: "897520998208"
+};
+
+export default firebaseConfig;
